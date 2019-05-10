0148143b6232ff89c5593d442d75a1f68c7c521b
Thomas Wenner <thmswenner@gmail.com>
2019-05-10 23:40:36 +0000

    Add object based javascript

--- music-library.js
+++ music-library.js
@@ -1,68 +1,28 @@
+function library (name, creator) {
+    this.name = name;
+    this.creator = creator;
+    this.playlist = [];
+    this.addPlaylist = (name) => {
+        this.playlist.push(new playlist(name))
+    }
+}
 
-// var library = {
+function playlist (name) {
+    this.name = name;
+    this.tracks = [];
+    this.addTrack = (title, rating, length) => {
+        this.tracks.push(new track(title, rating, length))
+    }
+}
 
-//     printPlaylists: function () {
-//       for (var key in this.playlists) {
-//         console.log(`${key} : ${this.playlists[key].name} - ${this.playlists[key].tracks.length} tracks`)
-//       }
-//     },
-  
-//     printTracks: function () {
-//       for (var key in this.tracks) {
-//         console.log(`${key} : ${this.tracks[key].name} by ${this.tracks[key].artist} (${this.tracks[key].album})`)
-//    }
-//     },
-  
-//     printPlaylist: function (playlistId) {
-//       var thisPlaylist = this.playlists[playlistId]
-//       console.log(`${thisPlaylist.id} : ${thisPlaylist.name} - ${thisPlaylist.tracks.length} tracks`)
-//       for (var trackId of thisPlaylist.tracks) {
-//         var thisTrack = this.tracks[trackId]
-//         console.log(`${thisTrack.id} : ${thisTrack.name} by ${thisTrack.artist} (${thisTrack.album})`)
-  
-//       }
-//     },
-  
-//     addTrackToPlaylist: function (trackId, playlistId) {
-//       this.playlists[playlistId].tracks.push(trackId)
-//     },
-  
-//     uid: function () {
-//       return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-//     },
-  
-//     addTrack: function (name, artist, album) {
-//       var trackUid = uid();
-//       this.tracks[trackUid] = {id: trackUid, name: name, artist: artist, album: album}
-//     },
-  
-//     addPlaylist: function (name) {
-//       var playListUid = uid();
-//       this.playlists[playListUid] = {id: playListUid, name: name}
-//     },
-  
-  
-//     tracks: { t01: { id: "t01",
-//                      name: "Code Monkey",
-//                      artist: "Jonathan Coulton",
-//                      album: "Thing a Week Three" },
-//               t02: { id: "t02",
-//                      name: "Model View Controller",
-//                      artist: "James Dempsey",
-//                      album: "WWDC 2003"},
-//               t03: { id: "t03",
-//                      name: "Four Thirty-Three",
-//                      artist: "John Cage",
-//                      album: "Woodstock 1952"}
-//             },
-//     playlists: { p01: { id: "p01",
-//                         name: "Coding Music",
-//                         tracks: ["t01", "t02"]
-//                       },
-//                  p02: { id: "p02",
-//                         name: "Other Playlist",
-//                         tracks: ["t03"]
-//                       }
-//                }
-//   }
-  
+function track (title, rating, length) {
+    this.title = title;
+    this.rating = rating;
+    this.songlength = length;
+}
+
+let newLibrary = new library('sexy beast', 'tom wenner')
+let track1 = new track("test", 3 , 30);
+let playlist1 = new playlist("myjamz");
+playlist1.tracks.push(track1);
+newLibrary.playlist.push(playlist1);
